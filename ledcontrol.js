d9ee989a0818e91caafaee2b24d97427c883a46e
Rene Schneider <slartie@posteo.de>
2025-02-05 01:17:16 +0100

    further development

---
+++ ledcontrol.js
@@ -0,0 +1,11 @@
+// Create a total of 1120 light divs and distribute them evenly along the 
+// lightpath svg path.
+function distributeLights() {
+  let lightpathSvg = document.querySelector('#lightpath>#lightpath0>path');
+  let lightpath = lightpathSvg[0];
+
+}
+
+
+
+distributeLights();
